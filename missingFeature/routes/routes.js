893a0e31998ba5ad1eccd55b4859cd3863749a61
leul1992 <leulbekele191@gmail.com>
2024-05-09 19:39:06 +0300

    add image fetch and fix routes

--- missingFeature/routes/routes.js
+++ missingFeature/routes/routes.js
@@ -1,9 +1,14 @@
+// Import necessary modules
 import express from 'express';
 import multer from 'multer';
 import { CreateMissingPerson } from "../controller/missingPersonController.js";
 
-const upload = multer();
-
+// Create an instance of Express Router
 export const routers = express.Router();
 
-routers.route('/createMissingPerson').post(upload.any(), CreateMissingPerson)
+// Initialize Multer middleware for file uploads
+const upload = multer();
+
+// Define route with middleware chain
+routers.route('/createMissingPerson').post( upload.any(), CreateMissingPerson);
+
