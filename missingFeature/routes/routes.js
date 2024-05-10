9c1ef69964e4d7602544cb6d7d2aca77e50901dc
leul1992 <leulbekele191@gmail.com>
2024-05-10 23:35:59 +0300

    add log data implementation

--- missingFeature/routes/routes.js
+++ missingFeature/routes/routes.js
@@ -2,6 +2,8 @@
 import express from 'express';
 import multer from 'multer';
 import { CreateMissingPerson } from "../controller/missingPersonController.js";
+import { AddActionLogGateWay, getActionLogByID, getAllActionLogs, getActionLogByUser } from '../controller/logging.js';
+import { logApiMiddleware } from '../middleware/logApi.js';
 
 // Create an instance of Express Router
 export const routers = express.Router();
@@ -9,6 +11,11 @@ export const routers = express.Router();
 // Initialize Multer middleware for file uploads
 const upload = multer();
 
-// Define route with middleware chain
+// Route for missing person
 routers.route('/createMissingPerson').post( upload.any(), CreateMissingPerson);
 
+// Route for Logging
+routers.route('/add_log_data').post(AddActionLogGateWay);
+routers.route('/get-action-logs').get(getAllActionLogs);
+routers.route('/get-action-log/:logId').get(getActionLogByID);
+routers.route('/get-user-action-log/:userId').get(getActionLogByUser);
