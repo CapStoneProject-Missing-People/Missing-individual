32cb25af8567eef9b84ee1ece833a4bc3c389d72
leul1992 <leulbekele191@gmail.com>
2024-05-16 23:17:28 +0300

    settle conflicting codes

--- missingFeature/routes/routes.js
+++ missingFeature/routes/routes.js
@@ -9,22 +9,22 @@ import { requireAuth } from "../middleware/authMiddleware.js";
 
 
 // Create an instance of Express Router
-export const routers = express.Router();
+export const routes = express.Router();
 
 // Initialize Multer middleware for file uploads
 const upload = multer();
 
 // Routes for missing person
-router
+routes
   .route("/createMissingPerson/:timeSinceDisappearance")
   .post(upload.any(), requireAuth, CreateMissingPerson);
 
 // Routes for Logging
-routers.route('/add_log_data').post(AddActionLogGateWay);
-routers.route('/get-action-logs').get(getAllActionLogs);
-routers.route('/get-action-log/:logId').get(getActionLogByID);
-routers.route('/get-user-action-log/:userId').get(getActionLogByUser);
+routes.route('/add_log_data').post(AddActionLogGateWay);
+routes.route('/get-action-logs').get(getAllActionLogs);
+routes.route('/get-action-log/:logId').get(getActionLogByID);
+routes.route('/get-user-action-log/:userId').get(getActionLogByUser);
 
 // Routes for Notification
-routers.route('/send-notification').post(sendPushNotification);
+routes.route('/send-notification').post(sendPushNotification);
 
