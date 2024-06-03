76daf792ffd649577f9c3893d4e7203d2ade7b0b
leul1992 <leulbekele191@gmail.com>
2024-06-03 23:16:10 +0300

    have separate face, description and overall match

---
+++ Frontend/missingpersonapp/lib/features/matchedCase/screens/face-match.dart
@@ -0,0 +1,11 @@
+// Dummy implementation of MissingPersonImageMatch
+import 'package:flutter/material.dart';
+
+class MissingPersonImageMatch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Missing Person Image Match Page'),
+    );
+  }
+}
---
+++ Frontend/missingpersonapp/lib/features/matchedCase/screens/overAllMatch.dart
@@ -0,0 +1,11 @@
+// Dummy implementation of MissingPersonImageMatch
+import 'package:flutter/material.dart';
+
+class OverAllMatch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Missing Person Over All Match Page'),
+    );
+  }
+}
