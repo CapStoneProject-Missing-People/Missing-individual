8ba3e6433409a025cf660201d76f7f25bacb6d03
leul1992 <leulbekele191@gmail.com>
2024-06-04 21:14:54 +0300

    remove unwated file and fix some issues

--- face-recognition/controller/faceMatch.js
+++ face-recognition/controller/faceMatch.js
@@ -1,41 +1,37 @@
 import FaceMatchResult from "../schema/faceMatch.js";
 
 export const FaceMatch = async (req, res) => {
+  console.log('entered to fetch missing match')
   try {
-    console.log(req.body.personId)
-    // Extract person_id from request (replace placeholder with your logic)
-    const personId = req.body.personId || req.query.personId; // Check body or query params
-
-    // Validate person_id presence and format
-    if (!personId || typeof personId !== 'string' || personId.trim() === '') {
-      return res.status(400).json({
-        success: false,
-        message: 'Invalid or missing person ID.',
-      });
-    }
-
-    // Fetch face match results from the database
-    const faceMatchResults = await FaceMatchResult.find({ person_id: personId });
-
-    if (!faceMatchResults.length) {
+    const { personIds } = req.body;
+    const matches = await FaceMatchResult.find({ person_id: { $in: personIds } });
+  
+    if (!matches.length) {
       return res.status(200).json({
         success: true,
         message: 'No face match results found.',
         facematch: [],
       });
-    }
-
-    // Respond with the face match results
-    res.status(200).json({
-      success: true,
-      message: 'Face match results retrieved successfully.',
-      facematch: faceMatchResults,
-    });
-  } catch (err) {
-    console.error('Error fetching face match results:', err);
-    res.status(500).json({
-      success: false,
-      message: 'An internal server error occurred while fetching face match results.',
-    });
+    };
+    
+  
+    const groupedMatches = matches.reduce((acc, match) => {
+      if (!acc[match.person_id]) {
+        acc[match.person_id] = [];
+      }
+      // Include desired properties in the response object
+      acc[match.person_id].push({
+        distance: match.distance,
+        similarity: match.similarity,
+        createdAt: match.createdAt,
+        isMatch: match.isMatch,
+        imageBuffer: match.imageBuffers
+      });
+      return acc;
+    }, {});
+  console.log(groupedMatches);
+    res.status(200).json({success: true,facematch: groupedMatches});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
